c0fd71e1f7816744e2a280960e6b0a0c122e3540
adamkaliszan <adam.kaliszan@gmail.com>
2023-07-23 16:36:46 +0200

    prepration for config file

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate flot;
 
 use btreemultimap::BTreeMultiMap;
 use cartesian::*;
-use clap::Parser;
-use mongodb::{bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion}, sync::Client};
+use clap::{Parser, Subcommand};
+use confy;
+use mongodb::{bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion, Credential}, sync::Client};
 use semver::Op::Less;
 use semver::{BuildMetadata, Prerelease, Version, VersionReq};
 use separator::Separatable;
+use serde::{Serialize, Deserialize};
 use serde_json::{Result, Value};
 use std::collections::{BTreeMap, LinkedList};
 use std::env::args;
@@ -16,6 +18,7 @@ use std::str::FromStr;
 use std::thread;
 use std::thread::{JoinHandle};
 use std::time::Instant;
+use mongodb::options::CredentialBuilder;
 
 use crate::sim::model::class::{Class, StreamType, sim_class::SimClass};
 use crate::sim::model::system::ModelDescription;
@@ -92,7 +95,7 @@ struct Cli {
 
     /// Number of threads
     #[clap(short, default_value_t=8)]
-    threads_no: usize,
+    threads_no: u32,
 
     /// Mongo URI
     #[clap(long, default_value="mongodb://192.168.1.39")]
@@ -100,14 +103,28 @@ struct Cli {
 
     /// Mongo database name
     #[clap(long, default_value="anystream")]
-    mongo_database: String
+    mongo_database: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MyConfig {
+    mongodb: String,
+    username: String,
+}
+
+impl ::std::default::Default for MyConfig {
+    fn default() -> Self {
+        Self {
+            mongodb: "192.168.1.39".into(),
+            username: "adam".into()
+        }
+    }
 }
 
 struct SimulationTask {
     tr_class: SimClass,
     v : usize,
     mim_state_cntr: u32,
-    version: Vec<u32>,
     sim_no: u32
 }
 
@@ -120,9 +137,12 @@ fn mongo_open_database(mongo_uri: &String, mongo_db: &String) -> Option<mongodb:
         }
     };
 
+    //let mut credential = Credential::builder().build();
+    //credential.username = Some(str!("adam"));
     // Set the server_api field of the client_options object to Stable API version 1
     let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
     client_options.server_api = Some(server_api);
+    //client_options.credential = Some(credential);
     // Create a new client and connect to the server
     let client = match Client::with_options(client_options) {
         Ok(cl) => cl,
@@ -158,108 +178,8 @@ fn read_finilized_statistics(model: &ModelDescription, db: &mongodb::sync::Datab
         .collect()
 }
 
-fn main() -> std::io::Result<()>
-{
-    let args = Cli::parse();
-
-    let mut db: Option<mongodb::sync::Database> = mongo_open_database(&args.mongo_uri, &args.mongo_database);
-
-    let mut file = File::create(args.output_path)?;
-    SimStatisticsMultiV::write_header(args.v, &mut file);
-
-    let call_streams = args.call_stream.clone();
-    let serv_streams = args.serv_stream.clone();
-    let cs_e2_d2_col:Vec<f64> = flot::range(args.cs_e2_d2_min, args.cs_e2_d2_max + args.cs_e2_d2_delta, args.cs_e2_d2_delta).collect();
-    let ss_e2_d2_col:Vec<f64> = flot::range(args.ss_e2_d2_min, args.ss_e2_d2_max + args.ss_e2_d2_delta, args.ss_e2_d2_delta).collect();
-    let a_col: Vec<f64> = flot::range(args.a_min, args.a_max + args.a_delta, args.a_delta).collect();
-
-    let mut no_off_skipped_classes = 0;
-    let mut no_off_stored_tasks_before = 0;
-    let mut no_off_total_tasks_before = 0;
-
-
-    let mut tasks:LinkedList<SimulationTask> = LinkedList::new();
-    let mut results: BTreeMultiMap<ModelDescription, StatisticsFinalized> = BTreeMultiMap::new();
-
-    for (cur_call_stream, cur_serv_stream, cs_e2_d2, ss_e2_d2, a) in
-        cartesian!(call_streams.iter(), serv_streams.iter(), cs_e2_d2_col.iter(), ss_e2_d2_col.iter(), a_col.iter())
-    {
-        // Prepare Streams and write its params
-        let call_stream = StreamType::from_str(&cur_call_stream.to_lowercase()).expect("Failed");
-        let service_stream = StreamType::from_str(&cur_serv_stream.to_lowercase()).expect("Failed");
-
-        if let Some(tr_class) = SimClass::new(
-            call_stream, service_stream,
-            *a, *cs_e2_d2, 1f64, *ss_e2_d2) {
-
-            for v in 1..args.v + 1 {
-                let model = ModelDescription{v:v as usize, class:tr_class.tr_class.clone()};
-                let mut sim_experiments = match &mut db {
-                    Some(_db) => {
-                        println!("Using mongo database");
-                        read_finilized_statistics(&model, &_db, args.mim_state_cntr)
-                    },
-                    None => LinkedList::new()
-                };
-
-                let no_of_ready_statistics= sim_experiments.len();
-
-                for sim_result in sim_experiments {
-                    results.insert(ModelDescription{class: tr_class.tr_class, v: v as usize}, sim_result);
-                }
-
-                no_off_stored_tasks_before += no_of_ready_statistics;
-                no_off_total_tasks_before += args.no_of_series;
-
-                for sim_no in no_of_ready_statistics..args.no_of_series {
-                    tasks.push_back(SimulationTask {
-                        tr_class: tr_class,
-                        v: v as usize,
-                        mim_state_cntr: args.mim_state_cntr,
-                        version: vec!(0, 0, 0),
-                        sim_no: (sim_no - no_of_ready_statistics) as u32
-                    });
-                }
-            }
-        } else {
-            no_off_skipped_classes += 1;
-        }
-    }
-    println!("Number od tasks to do: {}, number of stored (skipped) tasks {}", tasks.len(), no_off_stored_tasks_before);
-
-    let mut workers: LinkedList <JoinHandle<(ModelDescription, StatisticsFinalized)>> = LinkedList::new();
-    while !tasks.is_empty() {
-        let mut task_no = 0;
-        while task_no < args.threads_no && !tasks.is_empty() {
-            let cur_task = tasks.pop_front().unwrap();
-            workers.push_back(thread::spawn(move || {
-                println!("Simulation a={}, arrival stream {}:{}, service stream {}:{}", cur_task.tr_class.get_a(), cur_task.tr_class.get_str_new_desc(), cur_task.tr_class.get_new_e2d2(), cur_task.tr_class.get_str_end_desc(), cur_task.tr_class.get_end_e2d2());
-                let start = Instant::now();
-                let result = sim::simulation(cur_task.v, cur_task.tr_class, args.mim_state_cntr);
-                let duration = start.elapsed();
-                let pefromance = (result.no_of_events as f64) / duration.as_micros() as f64;
-                println!("v={}: performance {:.3} events/µs, no of events : {} ", cur_task.v, pefromance, result.no_of_events);
-                (ModelDescription{class:cur_task.tr_class.tr_class, v: cur_task.v }, result)
-            }));
-            task_no += 1;
-        }
-
-        while !workers.is_empty() {
-            let single_worker = workers.pop_front().unwrap();
-            let (key, value) = single_worker.join().unwrap();
-
-            if let Some(db_val) = &mut db {
-                let str = serde_json::to_string(&value).unwrap();
-                println!("Serialized: {str}");
-                if let Err(err) = value.write_mongo(&key, db_val) {
-                    println!("Failed to save results: {err}");
-                }
-            }
-            //result.write(&mut file);
-            results.insert(key, value);
-        }
-    }
-
+fn generate_csv(args: Cli, results: BTreeMultiMap<ModelDescription, StatisticsFinalized>) -> std::io::Result<()> {
+    let mut file = File::create(args.output_path.clone())?;
     SimStatisticsMultiV::write_header(args.v, &mut file);
 
     let mut final_results: BTreeMap<Class, SimStatisticsMultiV> = BTreeMap::new();
@@ -286,6 +206,120 @@ fn main() -> std::io::Result<()>
     }
 
     println!("Done");
+
     Ok(())
+}
+
+/// Prepares tasks.
+/// First check if the results are available in database
+fn prepare_tasks(args: &Cli, db: &Option<mongodb::sync::Database>, results: &mut BTreeMultiMap<ModelDescription, StatisticsFinalized>) -> LinkedList<SimulationTask> {
+    let mut tasks: LinkedList<SimulationTask> = LinkedList::new();
+
+    let call_streams = args.call_stream.clone();
+    let serv_streams = args.serv_stream.clone();
+    let cs_e2_d2_col:Vec<f64> = flot::range(args.cs_e2_d2_min, args.cs_e2_d2_max + args.cs_e2_d2_delta, args.cs_e2_d2_delta).collect();
+    let ss_e2_d2_col:Vec<f64> = flot::range(args.ss_e2_d2_min, args.ss_e2_d2_max + args.ss_e2_d2_delta, args.ss_e2_d2_delta).collect();
+    let a_col: Vec<f64> = flot::range(args.a_min, args.a_max + args.a_delta, args.a_delta).collect();
+
+    let mut no_off_skipped_classes = 0;
+    let mut no_off_stored_tasks_before = 0;
+    let mut no_off_total_tasks_before = 0;
+
+    for (cur_call_stream, cur_serv_stream, cs_e2_d2, ss_e2_d2, a) in
+    cartesian!(call_streams.iter(), serv_streams.iter(), cs_e2_d2_col.iter(), ss_e2_d2_col.iter(), a_col.iter())
+    {
+        // Prepare Streams and write its params
+        let call_stream = StreamType::from_str(&cur_call_stream.to_lowercase()).expect("Failed");
+        let service_stream = StreamType::from_str(&cur_serv_stream.to_lowercase()).expect("Failed");
+
+        if let Some(tr_class) = SimClass::new(
+            call_stream, service_stream,
+            *a, *cs_e2_d2, 1f64, *ss_e2_d2) {
+
+            for v in 1..args.v + 1 {
+                let model = ModelDescription{v:v as usize, class:tr_class.tr_class.clone()};
+                let mut sim_experiments = match &db {
+                    Some(_db) => read_finilized_statistics(&model, &_db, args.mim_state_cntr),
+                    None => LinkedList::new()
+                };
+
+                let no_of_ready_statistics= sim_experiments.len();
+                for sim_result in sim_experiments {
+                    results.insert(ModelDescription{class: tr_class.tr_class, v: v as usize}, sim_result);
+                }
+
+                no_off_stored_tasks_before += no_of_ready_statistics;
+                no_off_total_tasks_before += args.no_of_series;
+
+                for sim_no in no_of_ready_statistics..args.no_of_series {
+                    tasks.push_back(SimulationTask {
+                        tr_class: tr_class,
+                        v: v as usize,
+                        mim_state_cntr: args.mim_state_cntr,
+                        sim_no: (sim_no - no_of_ready_statistics) as u32
+                    });
+                }
+            }
+        } else {
+            no_off_skipped_classes += 1;
+        }
+    }
+    println!("Number od tasks to do: {}, number of stored (skipped) tasks {}", tasks.len(), no_off_stored_tasks_before);
+
+    tasks
+}
+
+fn calculate(no_of_threads:u32, mut tasks: LinkedList<SimulationTask>, db: &mut Option<mongodb::sync::Database>, results: &mut BTreeMultiMap<ModelDescription, StatisticsFinalized>) {
+    let mut workers: LinkedList <JoinHandle<(ModelDescription, StatisticsFinalized)>> = LinkedList::new();
+    while !tasks.is_empty() {
+        let mut task_no = 0;
+        while task_no < no_of_threads && !tasks.is_empty() {
+            let cur_task = tasks.pop_front().unwrap();
+            workers.push_back(thread::spawn(move || {
+                println!("Simulation a={}, arrival stream {}:{}, service stream {}:{}", cur_task.tr_class.get_a(), cur_task.tr_class.get_str_new_desc(), cur_task.tr_class.get_new_e2d2(), cur_task.tr_class.get_str_end_desc(), cur_task.tr_class.get_end_e2d2());
+                let start = Instant::now();
+                let result = sim::simulation(cur_task.v, cur_task.tr_class, cur_task.mim_state_cntr);
+                let duration = start.elapsed();
+                let pefromance = (result.no_of_events as f64) / duration.as_micros() as f64;
+                println!("v={}: performance {:.3} events/µs, no of events : {} ", cur_task.v, pefromance, result.no_of_events);
+                (ModelDescription{class:cur_task.tr_class.tr_class, v: cur_task.v }, result)
+            }));
+            task_no += 1;
+        }
+
+        while !workers.is_empty() {
+            let single_worker = workers.pop_front().unwrap();
+            let (key, value) = single_worker.join().unwrap();
+
+            if let Some(db_val) = db {
+                let str = serde_json::to_string(&value).unwrap();
+                println!("Serialized: {str}");
+                if let Err(err) = value.write_mongo(&key, db_val) {
+                    println!("Failed to save results: {err}");
+                }
+            }
+            results.insert(key, value);
+        }
+    }
+}
+
+fn main() -> std::io::Result<()>
+{
+    let args = Cli::parse();
+    let cfg = match confy::load(env!("CARGO_PKG_NAME")) {
+        Ok(config) => config,
+        Err(e) => {
+            println!("Failed to load config: {e}");
+            MyConfig::default()
+        }
+    };
+
+    let mut db: Option<mongodb::sync::Database> = mongo_open_database(&args.mongo_uri, &args.mongo_database);
+    let mut results: BTreeMultiMap<ModelDescription, StatisticsFinalized> = BTreeMultiMap::new();
+
+    let mut tasks = prepare_tasks(&args, &db, &mut results);
+    calculate(args.threads_no, tasks, &mut db, &mut results);
+
+    generate_csv(args, results)
 }
 
